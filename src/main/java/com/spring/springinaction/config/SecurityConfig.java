4ccdd08d5822dcb234227363a8a7f72cacb60770
usr <rlarlgus0206@naver.com>
2024-01-19 21:21:40 +0900

    update : SecurityConfig

--- src/main/java/com/spring/springinaction/config/SecurityConfig.java
+++ src/main/java/com/spring/springinaction/config/SecurityConfig.java
@@ -4,12 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
@@ -20,7 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -58,7 +53,7 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsManager users(DataSource dataSource) {
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user1")
@@ -71,5 +66,5 @@ public class SecurityConfig {
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
         users.createUser(user);
         return users;
-    }
+    }*/
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,11 @@
-delete from Taco_Order_Tacos;
-delete from Taco_Ingredients;
-delete from Taco;
-delete from Taco_Order;
+insert into users (username, password)
+values ('user1', 'password1');
+insert into users (username, password)
+values ('user2', 'password2');
 
-delete from Ingredient;
-insert into (id, name, type) values('FLTO','Flour Tortilla','WRAP');
-insert into (id, name, type) values('COTO','Corn Tortilla','WRAP');
-insert into (id, name, type) values('GRBF','Ground Beef','PROTEIN');
-insert into (id, name, type) values('CARN','Carnitas','PROTEIN');
-insert into (id, name, type) values('TMTO','Diced Tomatoes','VEGGIES');
-insert into (id, name, type) values('LETC','Lettuce','VEGGIES');
-insert into (id, name, type) values('CHED','Cheddat','CHEESE');
-insert into (id, name, type) values('JACK','Monterry Jack','CHEESE');
-insert into (id, name, type) values('SLSA','Salsa','SAUCE');
-insert into (id, name, type) values('SRCR','Sour Cream','SAUCE');
+insert into authorities (username, authority)
+values ('user1', 'ROLE_USER');
+insert into authorities (username, authority)
+values ('user2', 'ROLE_USER');
 
-
+commit;
